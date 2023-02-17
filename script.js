260527526f15914a2f0b2f574400ccfc80e9f8f3
Діма Якименко <akimenkodima154@gmail.com>
2023-02-17 09:33:04 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// Задача №4
+/*Оголоси змінну message і запиши в неї повідомлення про покупку,
+рядок у форматі: "You picked <назва товару>, price per item is <ціна товару> credits".
+Де < назва товару > і < ціна товару > — це значення змінних productName і pricePerItem.
+Використовуй синтаксис шаблонних рядків.
+*/
+
+//const productName = "Droid";
+//const pricePerItem= 3500;
+//const message = "You picked ${productName}, price per item is ${pricePerItem} credits";
+
+
+
+
+// Задача № 5
+// Магазин з продажу ремонтних дроїдів готовий до відкриття,
+// залишилося написати скрипт для їх замовлення.
+// Оголоси змінні та присвой їм відповідні значення:
+
+// pricePerDroid - ціна одного дроїда, значення 800
+// orderedQuantity - кількість дроїдів у замовленні, значення 6
+// deliveryFee - вартість доставки, значення 50
+// totalPrice - загальна сума замовлення до сплати, не забудь про вартість доставки
+// message - повідомлення про стан замовлення у форматі
+// "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price."
+
+//const pricePerDroid = 800;
+//const orderedQuantity = 6;
+//const deliveryFee = 50;
+//const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+//const message = "You ordered droids worth ${totalPrice} credits. Delivery ${deliveryFee} credits is included in total price.";
+
+
+// Задача № 6
+// Заміни вирази зі стандартними математичними операторами
+// на комбінований оператор присвоєння
+// з додаванням, відніманням, множенням і діленням.
+
+// Значення змінної a дорівнює 7
+// Використаний оператор +=
+
+// Значення змінної b дорівнює 6
+// Використаний оператор -=
+
+// Значення змінної c дорівнює 45
+// Використаний оператор *=
+
+// Значення змінної d дорівнює 2
+// Використаний оператор /=
+
+//let a = 5;
+//a + 2;
+//let b = 10;
+//b - 4;
+//let c = 15;
+//c * 3;
+//let d = 20;
+//d / 10;
